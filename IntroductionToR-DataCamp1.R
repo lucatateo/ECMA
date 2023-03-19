c06b6e85a70c04dde74e1897b31a5fc6176d4f22
Vale-LaMo <100314333+Vale-LaMo@users.noreply.github.com>
2023-03-19 23:22:46 +0100

    Updated repository for ECMA23

---
+++ IntroductionToR-DataCamp1.R
@@ -0,0 +1,90 @@
+##---- Ex.1 ----
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+
+
+##---- Ex.2 ----
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+
+
+# Modulo
+
+
+##---- Ex.3 - Variable assignment (1) ----
+# Assign the value 42 to x
+x <- 
+
+# Print out the value of the variable x
+x
+
+##---- Ex.4 - Variable assignment (2) ----
+# Assign the value 5 to the variable my_apples
+
+
+# Print out the value of the variable my_apples
+
+
+
+##---- Ex.5 - Variable assignment (3) ----
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+
+
+# Add these two variables together
+
+
+# Create the variable my_fruit
+
+
+
+##---- Ex.6 Apples and oranges ----
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- "six"
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit
+
+
+##---- Ex.7 Basic data types in R ----
+# Change my_numeric to be 42
+my_numeric <- 42.5
+
+# Change my_character to be "universe"
+my_character <- "some text"
+
+# Change my_logical to be FALSE
+my_logical <- TRUE
+
+
+##---- Ex.8 What's that data type? ----
+# Declare variables of different types
+my_numeric <- 42.5
+my_character <- "smart"
+my_logical <- TRUE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+
+
+# Check class of my_logical
+
